5330a53e5d4abf2cffca3e3e5880f6cc6a322bf8
Zesk06 <zesk06@gmail.com>
2018-06-13 06:50:45 +0200

    Restrict setup.py to python 3.6

--- setup.py
+++ setup.py
@@ -6,25 +6,19 @@ from setuptools import setup, find_packages
 #
 setup(
     #
-    name='imagebelt',
+    name="imagebelt",
     # la version du code
     version="0.0.3",
-
     packages=find_packages(),
-
     author="Nicolas Rouviere",
     author_email="infozesk@gmail.com",
     description="An image tool suite",
-    long_description=open('README.md').read(),
-
-    install_requires=['Pillow==4.0.0', ],
-
+    long_description=open("README.md").read(),
+    install_requires=["Pillow==4.0.0"],
     # Active la prise en compte du fichier MANIFEST.in
     include_package_data=True,
-
     #
-    url='http://github.com/zesk06/imagebelt',
-
+    url="http://github.com/zesk06/imagebelt",
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers.
     classifiers=[
         "Programming Language :: Python",
@@ -32,17 +26,10 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: French",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Multimedia :: Graphics",
     ],
-
-
     #
     #
-    entry_points={
-        'console_scripts': [
-            'imagebelt = imagebelt.imagebelt:main',
-        ],
-    },
+    entry_points={"console_scripts": ["imagebelt = imagebelt.imagebelt:main"]},
 )
