4b9f2e19365566f5b0a606f9b58629034fe49da4
Zesk06 <zesk06@gmail.com>
2018-06-13 06:40:42 +0200

    BLACK

--- imagebelt/imagebelt.py
+++ imagebelt/imagebelt.py
@@ -26,26 +26,25 @@ def main():
     """The main
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--verbose', '-v',
-                        action='store_true',
-                        help='goes to verbose', default=False)
-    subparsers = parser.add_subparsers(dest='subcommand')
+    parser.add_argument(
+        "--verbose", "-v", action="store_true", help="goes to verbose", default=False
+    )
+    subparsers = parser.add_subparsers(dest="subcommand")
 
-    reduce_parser = subparsers.add_parser('reduce', help='to reduce image')
-    reduce_parser.add_argument('folder', help='The folder to thumbnailize')
+    reduce_parser = subparsers.add_parser("reduce", help="to reduce image")
+    reduce_parser.add_argument("folder", help="The folder to thumbnailize")
     reduce_parser.set_defaults(func=reduce_f)
 
-    metadata_parser = subparsers.add_parser('db',
-                                            help='read metadata from JSON')
-    metadata_parser.add_argument('json', help='the json file')
+    metadata_parser = subparsers.add_parser("db", help="read metadata from JSON")
+    metadata_parser.add_argument("json", help="the json file")
     metadata_parser.set_defaults(func=json_to_image)
 
     args = parser.parse_args()
     if args.verbose:
         logging.root.setLevel(logging.DEBUG)
-        logging.info('Going to verbose')
+        logging.info("Going to verbose")
     logging.debug(args)
-    if not hasattr(args, 'func'):
+    if not hasattr(args, "func"):
         parser.print_usage()
         exit(1)
 
@@ -55,31 +54,38 @@ def main():
 def json_to_image(args):
     """Transfer json metadata to image EXIF data"""
     json_path = Path(args.json)
-    with open(json_path, 'r') as json_f:
+    with open(json_path, "r") as json_f:
         json_obj = json.load(json_f)
     parent_dir = json_path.parent
-    for media in json_obj['media']:
-        print(parent_dir / media['title'])
-        img_path = parent_dir / media['title']
-        title = media['description']
-        description = media['description']
-        tags = media['tags']
-        comments = ['%s:%s' % (com['author'], com['comment'])
-                    for com in media['comments']]
+    for media in json_obj["media"]:
+        print(parent_dir / media["title"])
+        img_path = parent_dir / media["title"]
+        title = media["description"]
+        description = media["description"]
+        tags = media["tags"]
+        comments = [
+            "%s:%s" % (com["author"], com["comment"]) for com in media["comments"]
+        ]
         lat_lon = None
-        if 'geoInfo' in media and media['geoInfo']:
-            latitude = media['geoInfo']['latitude_']
-            longitude = media['geoInfo']['longitude_']
+        if "geoInfo" in media and media["geoInfo"]:
+            latitude = media["geoInfo"]["latitude_"]
+            longitude = media["geoInfo"]["longitude_"]
             lat_lon = (latitude, longitude)
-        exiftool(img_path, title=title, description=description,
-                 tags=tags, comments=comments, lat_lon=lat_lon)
+        exiftool(
+            img_path,
+            title=title,
+            description=description,
+            tags=tags,
+            comments=comments,
+            lat_lon=lat_lon,
+        )
 
 
-def exiftool(image_path, title, description=None,
-             tags=None,
-             comments=None, lat_lon=None):
+def exiftool(
+    image_path, title, description=None, tags=None, comments=None, lat_lon=None
+):
     """Change image metadata using exiftool"""
-    cmd = ['exiftool', '%s' % image_path]
+    cmd = ["exiftool", "%s" % image_path]
     if title:
         cmd += ['-Title="%s"' % title]
     if description:
@@ -87,27 +93,29 @@ def exiftool(image_path, title, description=None,
         cmd += ['-ImageDescription="%s"' % description]
     if comments:
 
-        cmd += ['-UserComment="%s"' % '.'.join(comments)]
-        cmd += ['-XPComment="%s"' % '.'.join(comments)]
+        cmd += ['-UserComment="%s"' % ".".join(comments)]
+        cmd += ['-XPComment="%s"' % ".".join(comments)]
     if tags:
         for tag in [tag.strip() for tag in tags]:
             cmd += ['-Subject="%s"' % tag]
             cmd += ['-HierarchicalSubject="%s"' % tag]
             cmd += ['-Keywords="%s"' % tag]
     if lat_lon:
-        cmd += ['-GPSLatitude="%s"' % lat_lon[0],
-                '-GPSLongitude="%s"' % lat_lon[1]]
+        cmd += ['-GPSLatitude="%s"' % lat_lon[0], '-GPSLongitude="%s"' % lat_lon[1]]
     # to change date:
     # -alldates="20100101 080000" -filemodifydate="20100101 080000"
-    logging.debug('calling %s', cmd)
-    subprocess.call(cmd, cwd=image_path.parent,
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    logging.debug("calling %s", cmd)
+    subprocess.call(
+        cmd, cwd=image_path.parent, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
 
 
 def reduce_f(args):
     """reduce images within a folder"""
-    file_list = [os.path.join(args.folder, in_file)
-                 for in_file in glob.glob('%s/*.jpg' % args.folder)]
+    file_list = [
+        os.path.join(args.folder, in_file)
+        for in_file in glob.glob("%s/*.jpg" % args.folder)
+    ]
     pool = multiprocessing.Pool()
     pool.map(thumbnail, file_list)
 
@@ -116,25 +124,25 @@ def thumbnail(image_file, max_size=1200):
     """thumbnailize an image_file
     """
     size = max_size, max_size
-    back_file = os.path.splitext(image_file)[0] + '.backup'
-    outfile = os.path.splitext(image_file)[0] + '.jpg'
+    back_file = os.path.splitext(image_file)[0] + ".backup"
+    outfile = os.path.splitext(image_file)[0] + ".jpg"
     if not os.path.exists(back_file):
         try:
             my_image = Image.open(image_file)
             # shrink only if size if bigger
             if max(my_image.size) <= max_size:
-                print('smaller than requested %s' % image_file)
+                print("smaller than requested %s" % image_file)
                 return
-            print('back to %s' % back_file)
-            my_image.save(back_file, 'JPEG')
+            print("back to %s" % back_file)
+            my_image.save(back_file, "JPEG")
             my_image.thumbnail(size)
-            my_image.save(outfile, 'JPEG')
-            print('thmb to %s' % outfile)
+            my_image.save(outfile, "JPEG")
+            print("thmb to %s" % outfile)
         except IOError:
-            print('cannot create thumbnail for %s' % image_file)
+            print("cannot create thumbnail for %s" % image_file)
         return
-    print('already a backup file %s' % back_file)
+    print("already a backup file %s" % back_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
